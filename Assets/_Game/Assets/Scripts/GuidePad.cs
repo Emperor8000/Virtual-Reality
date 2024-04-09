cf12588587f4470f90fbcdbc7e20dab5ee4f17ec
Sean <sps20009@utdallas.edu>
2024-04-09 18:14:35 -0500

    Hints
    Added hint functionality to the guidepad

--- Assets/_Game/Assets/Scripts/GuidePad.cs
+++ Assets/_Game/Assets/Scripts/GuidePad.cs
@@ -6,9 +6,11 @@ using TMPro;
 public class GuidePad : MonoBehaviour
 {
     [SerializeField] private List<string> Steps = new List<string>();
+    [SerializeField] private List<string> Hints = new List<string>();
     private TextMeshProUGUI GuideText = null;
 
     private int currentStep = 0;
+    private bool hint = false;
     void Start()
     {
         if (!GuideText)
@@ -21,6 +23,7 @@ public class GuidePad : MonoBehaviour
         if(currentStep > 0)
         {
             currentStep--;
+            hint = false;
         }
         
         setText();
@@ -30,13 +33,35 @@ public class GuidePad : MonoBehaviour
         if(currentStep < Steps.Count-1)
         {
             currentStep++;
+            hint = false;
         }
         
         setText();
     }
 
+    public void toggleHint()
+    {
+        if (!hint)
+        {
+            hint = true;
+        }else if (hint)
+        {
+            hint = false;
+        }
+
+        setText();
+    }
+
     public void setText()
     {
-        GuideText.text = Steps[currentStep];
+        if (!hint)
+        {
+            GuideText.text = Steps[currentStep];
+        }
+        else if (hint)
+        {
+            GuideText.text = "Hint: " + Hints[currentStep];
+        }
+        
     }
 }
